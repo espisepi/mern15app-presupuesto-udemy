4603cc50c6ad4a0e7ce1a9069263fe5d8aabad59
espisepi <sepinaco@gmail.com>
2021-05-20 19:33:26 +0200

    Pregunta component: leyendo el presupuesto del usuario

--- src/componets/Pregunta.js
+++ src/componets/Pregunta.js
@@ -1,15 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Pregunta = () => {
+
+    /* Definir el state */
+    const [ cantidad, guardarCantidad ] = useState(0);
+
+    /* Funcion que lee el presupuesto */
+    const definirPresupuesto = e => {
+        /* e.target.value devuelve un string, por eso lo convertimos a numero */
+        guardarCantidad( parseInt(e.target.value, 10) );
+
+    }
+
+    /* Submit para definir el presupuesto */
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        /* Validar */
+        
+
+        /* Si se pasa la validacion */
+    }
+
+
     return (
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
 
-            <form>
+            <form
+                onSubmit={agregarPresupuesto}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 />
 
                 <input 
